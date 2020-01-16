33f810d3fae647b1befba9f0b7e038b6b934d056
monda <monda0524@gmail.com>
2020-01-16 23:35:05 +0900

    remove import_data.py

--- import_data.py
+++
@@ -1,7 +0,0 @@
-from sklearn.datasets import load_boston
-import pandas as pd
-
-def boston():
-    boston = load_boston()
-    boston_df = pd.DataFrame(boston.data, columns=boston.feature_names)
-    return boston_df
